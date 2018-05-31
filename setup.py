f5c4e2a4cc400138d53d72e84a9988d2a26db737
Jimmy Yang <r02521520@g.ntu.edu.tw>
2018-05-31 14:32:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #Resnet 3D model
 from setuptools import setup
 
-setup(name='resnet3d',
+setup(name='resnet3dtest',
       version='0.1',
       description='The funniest joke in the world',
       url='https://github.com/jimmy15923/test_pip',
