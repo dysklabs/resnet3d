c473beaf5b0f1294ebfdcea6970c2eadd98c7b08
TA / Jimmy <r02521520@g.ntu.edu.tw>
2018-05-31 11:25:29 +0800

    add resnet3d

---
+++ setup.py
@@ -0,0 +1,12 @@
+#Resnet 3D model
+from setuptools import setup
+
+setup(name='resnet3d',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/jimmy15923/test_pip',
+      author='Sa chi a jimmy',
+      author_email='jimmy15923@example.com',
+      license='MIT',
+      packages=['resnet3d'],
+      zip_safe=False)
